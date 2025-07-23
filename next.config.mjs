7a2265211fb1f93e93dcee0f6441ed959d9bc708
Guilherme Santos <guilherme.santos@NEOSYXADM.CORP>
2025-07-22 22:14:07 -0300

    ADD - Config para deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: false,
-  
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/smart-sales',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
